fa47e34e031cd493f435d5977992663f390cbe59
Evgeniy Vigurskiy <vigursky@ingenico.ru>
2015-12-04 16:51:09 +0300

    [+] Dummy commit. Added gps location on NumberFormatException

--- Calculator/app/src/main/java/com/peanutwolf/calculator/Calculator.java
+++ Calculator/app/src/main/java/com/peanutwolf/calculator/Calculator.java
@@ -5,9 +5,14 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.IBinder;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -20,7 +25,7 @@ import org.w3c.dom.Text;
 
 import java.util.Locale;
 
-public class Calculator extends Activity implements ServiceConnection {
+public class Calculator extends Activity implements ServiceConnection, LocationListener {
 
     private EditText firstArg;
     private EditText secondArg;
@@ -29,8 +34,17 @@ public class Calculator extends Activity implements ServiceConnection {
     private Button multiply;
     private Button divide;
     private TextView result;
+    private LocationManager locationManager;
+    Location location;
 
     private Calculate mCalcService;
+
+    // The minimum distance to change Updates in meters
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 1; // 10 meters
+
+    // The minimum time between updates in milliseconds
+    private static final long MIN_TIME_BW_UPDATES = 60 *1000 ; // 1 minute
+
 
     @Override
     protected void onStop() {
@@ -58,6 +72,26 @@ public class Calculator extends Activity implements ServiceConnection {
         multiply = (Button) findViewById(R.id.multiply);
         divide = (Button) findViewById(R.id.divide);
         result = (TextView) findViewById(R.id.result);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        boolean isGPSEnabled = locationManager
+                .isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        if (isGPSEnabled) {
+            if (location == null) {
+                locationManager.requestLocationUpdates(
+                        LocationManager.GPS_PROVIDER, MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                Log.d("GPS Enabled", "GPS Enabled");
+                if (locationManager != null) {
+                    location = locationManager
+                            .getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    if (location != null) {
+                        result.setText(String.valueOf(location.getAltitude()));
+                    }
+                }
+            }
+        }
 
 
         plus.setOnClickListener(new View.OnClickListener() {
@@ -96,10 +130,9 @@ public class Calculator extends Activity implements ServiceConnection {
             if (mCalcService != null)
                 result.setText(mCalcService.calculate(arg1, arg2, op));
         } catch (NumberFormatException e) {
-            result.setText(getString(R.string.format_exсept));
+            result.setText(String.valueOf(location.getAltitude()));
         }
     }
-
 
     @Override
     public void onServiceConnected(ComponentName name, IBinder service) {
@@ -109,5 +142,27 @@ public class Calculator extends Activity implements ServiceConnection {
     @Override
     public void onServiceDisconnected(ComponentName name) {
         mCalcService = null;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        result.setText(location.getAltitude()+"");
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Toast.makeText(this, "Enabled new provider " + provider,
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Toast.makeText(this, "Disable provider " + provider,
+                Toast.LENGTH_SHORT).show();
     }
 }
